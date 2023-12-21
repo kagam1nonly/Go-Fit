d13f5b981500675a0f2e406fe2de3e40d75af7ec
James Piastro <78834746+kagam1nonly@users.noreply.github.com>
2023-12-21 04:58:43 -0800

    Delete new directory

--- new/api/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from django.urls import reverse
-
-class AccountFormTests(TestCase):
-    def test_account_form_url(self):
-        response = self.client.get(reverse('account_form'))
-        self.assertEqual(response.status_code, 200)
